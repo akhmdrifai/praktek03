e878d10977a1e83c0e15daf618f3ca062be408f7
AKHMAD RIFAI <ahrifa28@gmail.com>
2018-04-08 04:41:39 +0800

    atribut dan methode

--- src/praktek03/kulkaspendingin.java
+++ src/praktek03/kulkaspendingin.java
@@ -1,4 +1,10 @@
 package praktek03;
 public class kulkaspendingin extends kulkas{
     
+    private String Pintu;
+    
+    void pendingin(){
+        System.out.println("string pintu......;");
+    }
+    
 }
