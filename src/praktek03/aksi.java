cb8771361b2dcb8c3f31490f7445e5787debbb06
AKHMAD RIFAI <ahrifa28@gmail.com>
2018-04-08 04:07:21 +0800

    instance, buat atribut dan panggilan method

--- src/praktek03/aksi.java
+++ src/praktek03/aksi.java
@@ -1,6 +1,12 @@
 package praktek03;
 public class aksi {
     public static void main(String[] args) {
+        kulkas t = new kulkas();
+        t.merek="LG";
+        t.harga=5000000;
+        t.jumlah=1;
         
+        
+        t.cetakInfo();
     }
 }
