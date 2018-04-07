ccc24ae48d87ef37e5f137cdbc455ad1c9c6382d
AKHMAD RIFAI <ahrifa28@gmail.com>
2018-04-08 03:51:40 +0800

    membuat atribut

--- src/praktek03/Praktek03.java
+++ src/praktek03/Praktek03.java
@@ -4,16 +4,8 @@
  * and open the template in the editor.
  */
 package praktek03;
-
-/**
- *
- * @author user
- */
 public class Praktek03 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
     }
---
+++ src/praktek03/kulkas.java
@@ -0,0 +1,7 @@
+package praktek03;
+public class kulkas {
+     String merek;
+     double harga;
+     double jumlah;
+    
+}
